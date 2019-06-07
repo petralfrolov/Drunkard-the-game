2303c99caab3dfe75241501d2753a38ed23429cb
dany <danyzolotarev@gmail.com>
2019-06-07 21:34:21 +0300

    Пиксельный шрифт

--- app/src/main/java/com/example/drunkard/ModeSelect.kt
+++ app/src/main/java/com/example/drunkard/ModeSelect.kt
@@ -1,16 +1,33 @@
 package com.example.drunkard
 
 import android.content.Intent
+import android.graphics.Point
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.mode_select.*
+import android.view.Display
+
+
 
 class ModeSelect : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mode_select)
+
+        /* Не пашет
+        val display = windowManager.defaultDisplay
+        val size = Point()
+        display.getSize(size)
+        val x = size.x
+        val y = size.y
+
+        pvpButton.height = y / 2
+        pvpButton.refreshDrawableState()
+        pveButton.refreshDrawableState()
+        */
+
         pveButton.setOnClickListener(::PvEgame)
 
     }
