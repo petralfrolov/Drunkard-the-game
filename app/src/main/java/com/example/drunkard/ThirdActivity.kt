992a8b517e772a90b300fee36183707cd96b09da
danyzolotarev <danyzolotarev@gmail.com>
2019-06-07 08:52:28 +0300

    function

--- app/src/main/java/com/example/drunkard/ThirdActivity.kt
+++ app/src/main/java/com/example/drunkard/ThirdActivity.kt
@@ -10,42 +10,43 @@ import com.example.drunkard.R
 import kotlinx.android.synthetic.main.activity_third.*
 
 class ThirdActivity : AppCompatActivity() {
+    var game = Drunkgame()
+    fun OnClickStartTurn(view: View) {
+        game.StartTurn()
+        /*s
+        Log.d("Check", game.yourCard.GetCardText())
+        Log.d("Check", yourCard.text.toString())
+        */
+        yourCard.text = "Your card: " + game.yourCard.GetCardText()
+        yourDeck.text = "Your deck: " + game.GetYourDeckSize().toString()
+        enemyCard.text = "Enemy's card: " + game.enemyCard.GetCardText()
+        enemyDeck.text = "Enemy's deck: " + game.GetEnemyDeckSize().toString()
+
+
+        //Thread.sleep(100)
+        if (!game.EndTurn()) {
+            if (game.GetYourDeckSize() <= 0) {
+                //здесь действия в случае победы
+            }
+            else {
+                //здесь действия в случае поражения
+            }
+        }
+        /*
+        yourCard.text = "Ваша карта: -"
+        enemyCard.text = "Карта противника: -"
+        yourDeck.text = "Ваша колода: " + game.GetYourDeckSize().toString()
+        enemyDeck.text = "Колода противника: " + game.GetEnemyDeckSize().toString()
+        */
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
-        var game = Drunkgame()
         yourDeck.text = "Your deck: " + game.GetYourDeckSize().toString()
         enemyDeck.text = "Enemy's deck: " + game.GetEnemyDeckSize().toString()
-        fun OnClickStartTurn(view: View) {
-            game.StartTurn()
-            /*
-            Log.d("Check", game.yourCard.GetCardText())
-            Log.d("Check", yourCard.text.toString())
-            */
-            yourCard.text = "Your card: " + game.yourCard.GetCardText()
-            yourDeck.text = "Your deck: " + game.GetYourDeckSize().toString()
-            enemyCard.text = "Enemy's card: " + game.enemyCard.GetCardText()
-            enemyDeck.text = "Enemy's deck: " + game.GetEnemyDeckSize().toString()
 
-
-            //Thread.sleep(100)
-            if (!game.EndTurn()) {
-                if (game.GetYourDeckSize() <= 0) {
-                    //здесь действия в случае победы
-                }
-                else {
-                    //здесь действия в случае поражения
-                }
-            }
-            /*
-            yourCard.text = "Ваша карта: -"
-            enemyCard.text = "Карта противника: -"
-            yourDeck.text = "Ваша колода: " + game.GetYourDeckSize().toString()
-            enemyDeck.text = "Колода противника: " + game.GetEnemyDeckSize().toString()
-            */
-
-        }
         button_start_turn.setOnClickListener(::OnClickStartTurn)
     }
 
